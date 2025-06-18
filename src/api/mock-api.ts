7a76bc1b1df98c35b0567616b9d02b6b4f37b5b5
Alejandro Miranda <amiranda110500@gmail.com>
2025-06-18 20:20:17 +0200

    add mock api with mocked response time

---
+++ src/api/mock-api.ts
@@ -0,0 +1,65 @@
+import { users } from './mock-data'
+
+export interface UserDto {
+  userId: string
+  name: string
+  username: string
+  email: string
+  userTin: string
+  phone: string
+  website?: string
+}
+
+export interface CreateUserDto {
+  name: string
+  username: string
+  email: string
+  userTin: string
+  phone: string
+  website?: string
+}
+
+export interface UpdateUserDto {
+  name?: string
+  username?: string
+  email?: string
+  userTin?: string
+  phone?: string
+  website?: string
+}
+
+export class UserMockApi {
+  public static async getUsers(): Promise<UserDto[]> {
+    const delay = Math.random() * 43000 + 2000 // Random delay between 2s and 45s
+    await new Promise((resolve) => setTimeout(resolve, delay))
+    return users
+  }
+
+  public static async getUserById(userId: string): Promise<UserDto | undefined> {
+    const delay = Math.random() * 43000 + 2000 // Random delay between 2s and 45s
+    await new Promise((resolve) => setTimeout(resolve, delay))
+    return users.find((user) => user.userId === userId)
+  }
+
+  public static async createUser(user: CreateUserDto): Promise<UserDto> {
+    const delay = Math.random() * 43000 + 2000 // Random delay between 2s and 45s
+    await new Promise((resolve) => setTimeout(resolve, delay))
+    const newUser = { ...user, userId: String(users.length + 1) }
+    users.push(newUser)
+    return newUser
+  }
+
+  public static async updateUser(
+    userId: string,
+    user: UpdateUserDto
+  ): Promise<UserDto | undefined> {
+    const delay = Math.random() * 43000 + 2000 // Random delay between 2s and 45s
+    await new Promise((resolve) => setTimeout(resolve, delay))
+    const index = users.findIndex((user) => user.userId === userId)
+    if (index === -1) {
+      return undefined
+    }
+    users[index] = { ...users[index], ...user }
+    return users[index]
+  }
+}
---
+++ src/api/mock-data.ts
@@ -0,0 +1,44 @@
+import type { UserDto } from './mock-api'
+
+export const users: UserDto[] = [
+  {
+    userId: '1',
+    name: 'John Doe',
+    username: 'johndoe',
+    email: 'john.doe@example.com',
+    userTin: '1234567890',
+    phone: '1234567890',
+  },
+  {
+    userId: '2',
+    name: 'Jane Doe',
+    username: 'janedoe',
+    email: 'jane.doe@example.com',
+    userTin: '1234567890',
+    phone: '1234567890',
+  },
+  {
+    userId: '3',
+    name: 'Jim Doe',
+    username: 'jimdoe',
+    email: 'jim.doe@example.com',
+    userTin: '1234567890',
+    phone: '1234567890',
+  },
+  {
+    userId: '4',
+    name: 'Jill Doe',
+    username: 'jilldoe',
+    email: 'jill.doe@example.com',
+    userTin: '1234567890',
+    phone: '1234567890',
+  },
+  {
+    userId: '5',
+    name: 'Jack Doe',
+    username: 'jackdoe',
+    email: 'jack.doe@example.com',
+    userTin: '1234567890',
+    phone: '1234567890',
+  },
+]
