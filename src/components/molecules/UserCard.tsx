2aef94977e42ffca51a5605860acc959b63354a0
Alejandro Miranda <amiranda110500@gmail.com>
2025-06-18 20:48:04 +0200

    add user table

--- src/components/molecules/UserCard.tsx
+++
@@ -1,9 +0,0 @@
-import type { UserDto } from '../../api/mock-api'
-
-interface Props {
-  user: UserDto
-}
-
-export const UserCard = ({ user }: Props) => {
-  return <div>{user.name}</div>
-}
--- src/components/organisms/UserList.tsx
+++ src/components/organisms/UserList.tsx
@@ -1,6 +1,12 @@
-import type { UserDto } from '../../api/mock-api'
-
-import { UserCard } from '../molecules/UserCard'
+import type { UserDto } from '@/api/mock-api'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table'
 
 interface Props {
   users: UserDto[]
@@ -8,10 +14,29 @@ interface Props {
 
 export const UserList = ({ users }: Props) => {
   return (
-    <div>
-      {users.map((user) => (
-        <UserCard key={user.userId} user={user} />
-      ))}
+    <div className='w-full overflow-x-auto m-2 border rounded-md'>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Name</TableHead>
+            <TableHead>Username</TableHead>
+            <TableHead>Email</TableHead>
+            <TableHead>Phone</TableHead>
+            <TableHead>TIN</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {users.map((user) => (
+            <TableRow key={user.userId}>
+              <TableCell>{user.name}</TableCell>
+              <TableCell>{user.username}</TableCell>
+              <TableCell>{user.email}</TableCell>
+              <TableCell>{user.phone}</TableCell>
+              <TableCell>{user.userTin}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
     </div>
   )
 }
